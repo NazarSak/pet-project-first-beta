2320250160a58fb69fcfc55611242254986d0817
NazarSak <112251832+NazarSak@users.noreply.github.com>
2023-09-17 00:00:42 +0300

    refresh

--- src/front-end/components/App.jsx
+++ src/front-end/components/App.jsx
@@ -2,7 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import { Homepage } from 'front-end/pages/homepage/Homepage';
 import { Workpage } from 'front-end/pages/workpage/WorkPage';
 import { Layout } from './layout/Layout';
-import { ContactPage } from 'front-end/pages/contactPage/ContatcPage';
+
 
 export const App = () => {
   return (
@@ -11,7 +11,7 @@ export const App = () => {
         <Route path="/" element={<Layout />}>
           <Route index element={<Homepage />} />
           <Route path="workPage" element={<Workpage />} />
-          <Route path="contactPage" element={<ContactPage />} />
+         
         </Route>
       </Routes>
     </>
