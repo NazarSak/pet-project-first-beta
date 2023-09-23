ecc813123e5317876ae0d256fcb33902be899aee
NazarSak <112251832+NazarSak@users.noreply.github.com>
2023-09-23 17:47:26 +0300

    add modalka

--- src/front-end/components/exitModalka/ExitModalka.jsx
+++ src/front-end/components/exitModalka/ExitModalka.jsx
@@ -1,13 +1,67 @@
-import React from 'react';
-import { ExitContainer, Logo, ExitButton } from '../header/header.styled';
+import React, { useState, useEffect } from 'react';
+import {
+  ExitContainer,
+  Logo,
+  ExitButton,
+  ModalWrapper,
+  ModalContent,
+} from './ExitModalka.styled';
 
 export const ExitModalka = ({ user, handleClick }) => {
+  const [isModalkaOpen, setIsModalkaOpen] = useState(false);
+
+  console.log(isModalkaOpen);
+
+  const OpenModalka = () => {
+    setIsModalkaOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalkaOpen(false);
+  };
+
+  useEffect(() => {
+    const handleKeyDown = e => {
+      if (e.key === 'Escape') {
+        closeModal();
+      }
+    };
+
+    const handleOutsideClick = e => {
+      if (e.target === e.currentTarget) {
+        closeModal();
+      }
+    };
+
+    if (isModalkaOpen) {
+      document.addEventListener('keydown', handleKeyDown);
+      document.addEventListener('click', handleOutsideClick);
+    } else {
+      document.removeEventListener('keydown', handleKeyDown);
+      document.removeEventListener('click', handleOutsideClick);
+    }
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+      document.removeEventListener('click', handleOutsideClick);
+    };
+  }, [isModalkaOpen]);
+
   return (
     <>
       <ExitContainer>
         <Logo>Welcome:{user}</Logo>
-        <ExitButton onClick={handleClick}> Exit</ExitButton>
+        <ExitButton onClick={OpenModalka}>Exit</ExitButton>
       </ExitContainer>
+      {isModalkaOpen && (
+        <ModalWrapper onClick={closeModal}>
+          <ModalContent>
+            <h2>Шо ти тут забуло бидло</h2>
+            <p>Теряйся отсюда</p>
+            <button onClick={handleClick}>Піти нахер</button>
+          </ModalContent>
+        </ModalWrapper>
+      )}
     </>
   );
 };
